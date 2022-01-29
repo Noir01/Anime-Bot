8d14217be9f220a71ac64b58acfd1cba961e0f50
Noir <63553175+Noir01@users.noreply.github.com>
2022-01-29 19:55:34 +0530

    Added link

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
-from discord.commands import Option, slash_command
+from discord.commands import Option, slash_command, permissions
 import psycopg
 from traceback import format_exc
 from os import environ
+from requests import post
 
 sqlCommands = [
     "ADD",
@@ -92,16 +93,81 @@ def get_sql_commands(ctx: discord.AutocompleteContext):
         command for command in sqlCommands if ctx.value.lower() in command.lower()
     ]
 
-@Client.slash_command()
-async def execute(ctx: discord.ApplicationContext, query: Option(str, "SQL Query to run", autocomplete=get_sql_commands)):
-    conn = psycopg.connect("postgresql://postgres:8Da2vF3ky6X2G7yBkhNZ@containers-us-west-1.railway.app:8039/railway")
+@Client.event
+async def on_ready():
+    print("Online now")
+    global conn
+    conn = psycopg.connect(postgressURL)
+    conn.autocommit = True
+
+@Client.slash_command(description="Admin-level command to manually edit database.", default_permission=False)
+@permissions.is_user(629243339379834880)
+async def execute(ctx: discord.ApplicationContext, query: Option(str, "SQL Query to run", autocomplete=discord.utils.basic_autocomplete(get_sql_commands))):
+    global conn
     with conn.cursor() as curr:
         try:
             curr.execute(query)
-            await ctx.send(curr.fetchall())
+            await ctx.respond(curr.fetchall())
         except psycopg.errors.ActiveSqlTransaction:
             await ctx.respond("psycopg.errors.ActiveSqlTransaction: CREATE DATABASE cannot run inside a transaction block")
         except BaseException:
             await ctx.respond(format_exc())
+
+class LinkConfirm(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+        self.timeout = 300.0
+
+    @discord.ui.button(emoji="<:AYes:765142287902441492>", style=discord.ButtonStyle.green)
+    async def confirm(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = True
+        self.stop()
+
+    @discord.ui.button(emoji="<:ANo:765142286681112576>", style=discord.ButtonStyle.danger)
+    async def cancel(self, button: discord.ui.Button, interaction: discord.Interaction):
+        self.value = False
+        self.stop()
+
+@Client.slash_command(description="Used to link an Anilist profile to your Discord profile.")
+async def link(ctx: discord.ApplicationContext, username: Option(str, "Username of the anilist account you want to link.")):
+    await ctx.defer()
+    query = '''
+query ($name: String, $page: Int, $perPage: Int) {
+  Page(page: $page, perPage: $perPage) {
+    users(name: $name) {
+      id
+      name
+      siteUrl
+    }
+  }
+}
+    '''
+    variables = {'name': username, 'page': 1, 'perPage': 1}
+    url = 'https://graphql.anilist.co'
+    response = post(url,json={'query': query,'variables': variables}).json()
+    users = response['data']['Page']['users']
+    if not users:
+        await ctx.respond("No matching [Anilist](https://anilist.co) accounts found.")
+        return
+    user = users[0]
+    global conn
+    with conn.cursor() as curr:
+        curr.execute("SELECT * from discord_anilist")
+        for pair in curr.fetchall():
+            if pair[1] == user['id']:
+                await ctx.respond(f"[{user['name']}](https://anilist.co/user/{user['name']}) is already registered under another discord account.")
+                return
+        view = LinkConfirm()
+        question = await ctx.respond(f"Is [{user['name']}]({user['siteUrl']}) the account you want to link?", view=view)
+        await view.wait()
+        if view.value is None:
+            await question.edit("Timed out.")
+        elif view.value:
+            curr.execute(f"DELETE FROM discord_anilist WHERE discord={ctx.author.id}")
+            curr.execute(f"INSERT INTO discord_anilist VALUES ({ctx.author.id}, {user['id']})")
+            await question.edit(f"Successfully registered [{user['name']}](https://anilist.co/user/{user['name']}).")
+        elif not view.value:
+            await question.edit("Please try again with your username.")
 
 Client.run(environ['TOKEN'])
