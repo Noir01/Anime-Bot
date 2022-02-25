cc31d6f975cfc1f45c6877b7e9b274af5fcb2085
Noir <63553175+Noir01@users.noreply.github.com>
2022-02-25 07:29:18 +0530

    No need for a thread anymore

--- Cogs/IndianTeenAnimeThread.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-
-class IndianTeenAnimeThread(commands.Cog):
-    def __init__(self, Client) -> None:
-        self.Client = Client
-
-def setup(Client):
-    Client.add_cog(IndianTeenAnimeThread(Client))
