03ab5fc9abec23f32cdbbb26ad40e108e5c5ac28
Noir <63553175+Noir01@users.noreply.github.com>
2022-06-05 15:09:46 +0530

    Added cog load/reload/unload capabilities

--- Cogs/Mod.py
+++ Cogs/Mod.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 from traceback import format_exc
 
+
 class Mod(commands.cog):
     def __init__(self, bot: commands.bot):
         self.bot = bot
@@ -27,3 +28,39 @@ class Mod(commands.cog):
                         os.remove("result.txt")
                 except BaseException:
                     await ctx.send(format_exc())
+
+    @commands.command(hidden=True)
+    async def load(self, ctx: commands.Context, *, module: str):
+        """Loads a module."""
+        if ctx.author.id != 629243339379834880 and ctx.author.id != 497352662451224578:
+            return
+        try:
+            await self.Client.load_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
+
+    @commands.command(hidden=True)
+    async def unload(self, ctx: commands.Context, *, module: str):
+        """Unloads a module."""
+        if ctx.author.id != 629243339379834880 and ctx.author.id != 497352662451224578:
+            return
+        try:
+            await self.Client.unload_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
+
+    @commands.group(name="reload", hidden=True, invoke_without_command=True)
+    async def _reload(self, ctx: commands.Context, *, module: str):
+        """Reloads a module."""
+        if ctx.author.id != 629243339379834880 and ctx.author.id != 497352662451224578:
+            return
+        try:
+            await self.Client.reload_extension(module)
+        except commands.ExtensionError as e:
+            await ctx.send(f"{e.__class__.__name__}: {e}")
+        else:
+            await ctx.send("\N{OK HAND SIGN}")
