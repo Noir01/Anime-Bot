c844044bf4c6d1a1071d853e9f771d2e0bd00c81
Noir <63553175+Noir01@users.noreply.github.com>
2022-06-14 20:50:23 +0530

    Added support for logging and removed auto syncing

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import asyncio
+import logging
 from os import environ
 
 import aiohttp
 import discord
+import requests
 from discord.ext import commands
 from psycopg_pool import AsyncConnectionPool
 
-# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) 
-#Postgres needs this to run on Windows
+# asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+# Postgres needs this to run on Windows
+
+
+class WebhookHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        logEntry = self.format(record)
+        return requests.post(url=environ['WEBHOOK_URL'], json={"content": logEntry})
+
+
+class WebhookFormatter(logging.Formatter):
+    def formatTime(self, record: logging.LogRecord, datefmt: str | None = ...) -> str:
+        return f"<t:{int(record.created)}:f>"
 
 
 class Animebot(commands.Bot):
@@ -23,14 +35,17 @@ class Animebot(commands.Bot):
         self.pool = AsyncConnectionPool(conninfo=environ["DATABASE_URL"])
         async with self.pool.connection() as conn:
             async with conn.cursor() as curr:
-                await curr.execute("""CREATE TABLE IF NOT EXISTS general (discord bigint PRIMARY KEY,
+                await curr.execute(
+                    """CREATE TABLE IF NOT EXISTS general (discord bigint PRIMARY KEY,
                                                                           anilist int UNIQUE,
-                                                                          anime JSON, manga JSON)""")
-                await curr.execute("""CREATE TABLE IF NOT EXISTS discord_anilist (discord bigint PRIMARY KEY,
-                                                                                  anilist int UNIQUE)""")
+                                                                          anime JSON, manga JSON)"""
+                )
+                await curr.execute(
+                    """CREATE TABLE IF NOT EXISTS discord_anilist (discord bigint PRIMARY KEY,
+                                                                                  anilist int UNIQUE)"""
+                )
         for ext in self.initial_extensions:
             await self.load_extension(ext)
-        await self.tree.sync()
 
     async def close(self):
         await super().close()
@@ -42,4 +57,4 @@ class Animebot(commands.Bot):
 
 
 bot = Animebot()
-bot.run(environ["TOKEN"])
+bot.run(environ["TOKEN"], log_handler=WebhookHandler(level=20), log_formatter=WebhookFormatter("[{asctime}] [{levelname:<8}] {name}: {message}", style="{"))
